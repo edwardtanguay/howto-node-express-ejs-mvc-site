90f8be652765b18a6acb0be41fb1a5776c340345
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-20 07:32:10 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import { siteData } from './src/models.js';
+import { siteView } from './src/views.js';
+import path from 'path';
+
+const app = express();
+const port = process.env.PORT || 3007;
+const __dirname = path.resolve(path.dirname(''));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './public/views'));
+
+app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//     res.send(siteView(siteData));
+// });
+
+app.get('/', (req, res) => {
+    res.render('pages/index', siteData)
+});
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+});
